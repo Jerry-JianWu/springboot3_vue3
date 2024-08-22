728a88d4701ee2ee843d581cabb83976ca2ded9f
jianwu <1012245670@qq.com>
2024-08-22 10:51:32 +0800

    Add UserController login logic without jwt token

--- src/main/java/com/itteach/controller/UserController.java
+++ src/main/java/com/itteach/controller/UserController.java
@@ -3,6 +3,7 @@ package com.itteach.controller;
 import com.itteach.pojo.Result;
 import com.itteach.pojo.User;
 import com.itteach.service.UserService;
+import com.itteach.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -29,5 +30,22 @@ public class UserController {
         }else{
             return Result.error("用户名已被占用");
         }
+    }
+    @PostMapping("/login")
+    public Result<String> login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password){
+        // 根据用户名查询用户
+        User loginUser = userService.findByUserName(username);
+        //判断该用户是否存在
+        if(loginUser == null){
+            return Result.error("用户名错误");
+        }
+        //判断密码是否正确,loginUser对象中的password是密文，所以同样要对这个password加密在判断
+        if(Md5Util.getMD5String(password).equals(loginUser.getPassword())){
+            // 登陆成功
+            return Result.success("jwt token令牌：");
+        }
+
+        return Result.error("密码错误");
+
     }
 }
