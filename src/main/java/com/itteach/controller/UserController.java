d35a2f381c35b8c1acf572038cdc96a4957f5a22
jianwu <1012245670@qq.com>
2024-08-22 10:37:26 +0800

    exceptionhandler

--- src/main/java/com/itteach/controller/UserController.java
+++ src/main/java/com/itteach/controller/UserController.java
@@ -3,18 +3,21 @@ package com.itteach.controller;
 import com.itteach.pojo.Result;
 import com.itteach.pojo.User;
 import com.itteach.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public Result register(String username, String password){
+    public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password){
         // finByUserName
         User u = userService.findByUserName(username);
         if(u == null){
