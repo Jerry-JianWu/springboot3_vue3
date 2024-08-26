3eb9bb773ca57e3766d5d195ac6ed16405598833
jianwu <1012245670@qq.com>
2024-08-26 15:04:40 +0800

    文章列表

--- src/main/java/com/itteach/service/CategoryService.java
+++ src/main/java/com/itteach/service/CategoryService.java
@@ -2,7 +2,11 @@ package com.itteach.service;
 
 import com.itteach.pojo.Category;
 
+import java.util.List;
+
 public interface CategoryService {
     // 新增分类
     void add(Category category);
+    // 列表查询
+    List<Category> list();
 }
